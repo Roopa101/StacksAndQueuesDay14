98f48083c82b243a2906c223a69c1b0db29c4174
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-10 19:21:15 +0530

    [Roopa] EnQueue Elements in the Queue

--- StacksQueues/StacksQueues/Program.cs
+++ StacksQueues/StacksQueues/Program.cs
@@ -12,15 +12,11 @@ namespace StacksQueues
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.Push(70);
-            linkedListStack.Push(30);
-            linkedListStack.Push(56);
-            linkedListStack.Display();
-            linkedListStack.Peek();// top.data
-            linkedListStack.Pop();
-            linkedListStack.IsEmpty();
-            linkedListStack.Display();
+            Queue linkedListQueue = new Queue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();// this.top=this.top.next
             Console.ReadKey();
         }
     }
